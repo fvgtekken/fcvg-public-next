a5920677a983bea875bc847433cf7c1f89630a09
federico <federicovongromann@gmail.com>
2024-05-02 10:00:03 -0300

    Add folder structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# React + TypeScript + Vite
+First test
+https://www.bogotobogo.com/DevOps/Docker/Docker-Compose-Nginx-Reverse-Proxy-Multiple-Containers.php
+Test
+
+
+echo $CR_PAT | docker login ghcr.io -u fvgtekken --password-stdin
+
+docker build . -t ghcr.io/fvgtekken/fcvg-public-next:latest && docker push ghcr.io/fvgtekken/fcvg-public-next:latest
---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com'
+      }
+
+    ]
+  }
+
+}
+
+module.exports = nextConfig
