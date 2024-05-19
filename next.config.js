fc668cf2313b247fa769050fc2b9535abfc06416
federico <federicovongromann@gmail.com>
2024-05-19 16:09:18 -0300

    Add fix3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- output: 'standalone'
+ output: 'standalone',
+   async rewrites() {
+    return [
+      {
+        source: `/api/:path*`,
+        destination: `/api/:path*`,
+      },
+      {
+        source: `/:path*`,
+        destination: `https://srv498956.hstgr.cloud/:path*`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
+
