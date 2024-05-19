5b4224b33954b7a5c51b6404e48cc76a8971ad31
federico <federicovongromann@gmail.com>
2024-05-19 16:22:42 -0300

    Add new config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  output: 'standalone',
-   async rewrites() {
-    return [
-      {
-        source: `/api/:path*`,
-        destination: `/api/:path*`,
-      },
-      {
-        source: `/:path*`,
-        destination: `https://srv498956.hstgr.cloud/:path*`,
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
